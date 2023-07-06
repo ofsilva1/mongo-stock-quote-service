f9438752328b8de20890e7cb2e5ccbc63b820b82
Omar Silva <abettermanep@gmail.com>
2023-07-06 15:02:55 -0600

    Add generator service

---
+++ src/main/java/guru/springframework/mongostockquoteservice/service/QuoteGeneratorService.java
@@ -0,0 +1,12 @@
+package guru.springframework.mongostockquoteservice.service;
+
+
+import guru.springframework.mongostockquoteservice.model.Quote;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+
+public interface QuoteGeneratorService {
+
+    Flux<Quote> fetchQuoteStream(Duration period);
+}
